6baa7383adce12792eb542ccd8f2d0b1ca603a0a
email <2399761775@qq.com>
2022-07-10 10:10:02 +0800

    这是我的第三次word提交

--- src/main/java/com/guo/FindQqApplication.java
+++ src/main/java/com/guo/FindQqApplication.java
@@ -8,7 +8,7 @@ public class FindQqApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FindQqApplication.class, args);
-        System.out.println("我只能说我服了");
+        int i=0;
+        System.out.println(i);
     }
-
 }
